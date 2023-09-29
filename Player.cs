27be0ab512a443fefebe984e8419308d32ca85a6
KAEduard <145798804+KAEduard@users.noreply.github.com>
2023-09-29 14:28:01 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace PIDORAS
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Point { get; set; }
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
